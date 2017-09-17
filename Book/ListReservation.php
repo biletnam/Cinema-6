fc9d60e34002ee14656eb396771fc0cb07a92e63
Keram <sirkeram@gmail.com>
2017-09-17 19:45:52 +0200

    Block reserved seat

---
+++ Book/ListReservation.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marek
+ * Date: 12.09.17
+ * Time: 22:08
+ */
+
+class ListReservation
+{
+    private $listReservation;
+    private $base;
+    private $idSeance;
+
+    public function __construct($idSeance = 1)
+    {
+        require_once "Reservation.php";
+        $this->listReservation = array();
+
+        $this->idSeance = $idSeance;
+    }
+
+    public function setList()
+    {
+        require_once "Base/Base.php";
+        require_once "Base/Select.php";
+        $this->base = new Select();
+        $quary = $this->base->query("Booking", "IdSeance = $this->idSeance", "FreeSeat");
+
+        if($quary->num_rows > 0)
+        {
+            while ($line = $quary->fetch_assoc())
+            {
+                $this->addElementList($line['FreeSeat']);
+            }
+        }
+        $this->base->closeBase();
+    }
+
+    private function addElementList($seat)
+    {
+        array_push($this->listReservation, $seat);
+    }
+
+    public function getList()
+    {
+        return $this->listReservation;
+    }
+}
+?>
