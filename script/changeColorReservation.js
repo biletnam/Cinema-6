3abd2d5a2d2adec37f316968cae55df83535b5ad
SirKeram <sirkeram@gmail.com>
2017-09-26 21:27:23 +0200

    unchose one seat

--- script/changeColorReservation.js
+++ script/changeColorReservation.js
@@ -1,11 +1,19 @@
 var idCurrent = null;
 
-function changeColorSeatSit(id) {
+function changeColorSeatSit(id)
+{
     if(idCurrent != null)
     {
         idCurrent.style.backgroundColor = '#42EEF4';
     }
+    if(idCurrent == id)
+    {
+        idCurrent.style.backgroundColor = '#42EEF4';
+    }
+    else
+    {
+        id.style.backgroundColor = '#00FF00';
+    }
 
     idCurrent = id;
-    idCurrent.style.backgroundColor = '#00FF00';
 }
