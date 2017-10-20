40480fc3c263c3b9928309c98c82e765fa1faa8b
SirKeram <sirkeram@gmail.com>
2017-10-20 12:32:23 +0200

    clean code

--- User.php
+++ User.php
@@ -3,14 +3,69 @@
  * Created by PhpStorm.
  * User: marek
  * Date: 10/1/17
- * Time: 5:21 PM
+ * Time: 4:41 PM
  */
 
-class User extends Person
+abstract class Person
 {
-    public function __construct($row)
+    private $id;
+    private $nick;
+    private $password;
+    private $email;
+    private $dignity;
+
+    protected function __construct($row)
     {
-        parent::__construct($row);
+        $this->id = $row['IdUser'];
+        $this->nick = $row['Login'];
+        $this->password = $row['Pass'];
+        $this->email = $row['Email'];
+        $this->dignity = $row['NameSurname'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNick()
+    {
+        return $this->nick;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDignity()
+    {
+        return $this->dignity;
+    }
+
+    public function updateUser($email, $dignity)
+    {
+        $this->email = $email;
+        $this->dignity = $dignity;
     }
 }
-?>
